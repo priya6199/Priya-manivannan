348c6905f6dc4e8c8270a961c34e480983e97aad
priya6199 <52281605+priya6199@users.noreply.github.com>
2020-01-08 15:00:13 +0530

    Create arithmetic operation.py

---
+++ Python/arithmetic operation.py
@@ -0,0 +1,20 @@
+a,b =map( int,input().split())
+
+add = a + b 
+
+sub = a - b 
+
+mul = a * b 
+  
+div1 = a / b 
+  
+div2 = a // b 
+
+mod = a % b 
+  
+print(add) 
+print(sub) 
+print(mul) 
+print(div1) 
+print(div2) 
+print(mod) 
